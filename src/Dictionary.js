541e6e0b3410fbcca1f68cddc8baefc59cfbbc3c
Mocca24 <signupcyn@gmail.com>
2023-12-27 12:22:58 +0200

    Meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,22 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary(){
     let [word, setWord] = useState("");
+    let [results, setResults] = useState (null);
 
     function handleResponse(response){
-        console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
     function search(event){
         event.preventDefault();
 
-        let key = "8a84a250d94e12ocbbetef4209917359";
+        let apiKey = "8a84a250d94e12ocbbetef4209917359";
 
-        let apiUrl=`https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${key}`;
+        let apiUrl=`https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
         axios.get(apiUrl).then(handleResponse);
     }
 
@@ -25,9 +27,12 @@ export default function Dictionary(){
 
     return (
     <div className="Dictionary">
-        <form onSbmit={search}>
+        <form onSubmit={search}>
         <input type="search" onChange={handleWordChange}/>
         </form>
+
+        <Results results = {results}/> 
+
     </div>
     );
 }
