289f127b78ac9c43c9b54c80ce3e24825f77d9cc
Mocca24 <signupcyn@gmail.com>
2023-12-31 11:38:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ export default function App() {
           <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
-          <small>Coded by <a href="https://github.com/Mocca24/dictionary" target="_blank">Cynthia Alvez</a></small>
+          <small>Coded by <a href="https://github.com/Mocca24/dictionary" target="_blank"
+          rel="noreferrer">Cynthia Alvez</a></small>
         </footer>
       </div>
     </div>
